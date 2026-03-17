b5b8ebc48632576077d9982c75b240641af2f496
CarlosHerrera1210 <ch7690362@gmail.com>
2026-03-16 19:05:21 -0500

    feat: add main menu navigation

--- menu_interactivo_c#/Program.cs
+++ menu_interactivo_c#/Program.cs
@@ -6,3 +6,56 @@ class Program
     {
         ShowMainMenu();
     }
+
+
+    static void ShowMainMenu()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("===== SISTEMA DE BIBLIOTECA =====");
+            Console.WriteLine("1. Libros");
+            Console.WriteLine("2. Usuarios");
+            Console.WriteLine("3. Préstamos");
+            Console.WriteLine("4. Búsquedas y reportes");
+            Console.WriteLine("5. Guardar / Cargar datos");
+            Console.WriteLine("6. Salir");
+
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "1":
+                    ShowBooksMenu();
+                    break;
+
+                case "2":
+                    ShowUsersMenu();
+                    break;
+
+                case "3":
+                    ShowLoansMenu();
+                    break;
+
+                case "4":
+                    ShowSearchReportsMenu();
+                    break;
+
+                case "5":
+                    ShowPersistenceMenu();
+                    break;
+
+                case "6":
+                    exit = ConfirmExitAndSave();
+                    break;
+
+                default:
+                    Console.WriteLine("Opción inválida");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
